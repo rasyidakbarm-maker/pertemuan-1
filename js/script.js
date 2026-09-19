41a2924741aabf733c3995fffc1e3fbc50520e0a
Rasyid Akbar Maulana <rasyidakbarm@gmail.com>
2026-09-19 21:42:21 +0700

    website umkm kopi kampung

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
   promoButton.textContent = "Promo: Beli 3 Gratis 1!";
-  console.log("Promo Kopi Nusa berhasil ditampilkan.");
+  console.log("Promo Kopi Kampung berhasil ditampilkan.");
 });
